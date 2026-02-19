5020685e119cf689bbf5394a8da7a1873cacfc3d
NatYarm <yarmnat7@gmail.com>
2026-02-19 22:34:01 +0100

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+import { dependencies, devDependencies } from './package.json'
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+      name: 'Joyfy_UI_Kit',
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
 })
